85a33f65ded8ecb4184bf130b6305c2cb28f8510
Adem <adem.benzineb.dev@gmail.com>
2025-05-12 18:37:27 +0100

    Starting the project
    just adding html and css code and some variables
    in javascript

---
+++ script.js
@@ -0,0 +1,10 @@
+const bt = document.querySelector("#bt");
+bt.addEventListener("click", Add() );
+
+function Add() {
+    const div = document.querySelector("#items");
+    const input = document.getElementById("item").value;
+
+    new_item = document.createElement("div")``
+    
+}
