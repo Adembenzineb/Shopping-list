9af5cf864c037b48345feec959a43f37871d3337
Adem <adem.benzineb.dev@gmail.com>
2025-05-13 10:50:28 +0100

    Adding the Add script without css

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
-const bt = document.querySelector("#bt");
-bt.addEventListener("click", Add() );
+const bt = document.querySelector("#addItem");
+bt.addEventListener("click", Add);
 
 function Add() {
     const div = document.querySelector("#items");
     const input = document.getElementById("item").value;
 
-    new_item = document.createElement("div")``
+
+    new_item = document.createElement("div")
+    new_item.classList.Add("item")
+    new_item.style["backgroundColor"] ="pink"
+    div.appendChild(new_item)
+
+    button = document.createElement("input")
+    button.classList.Add("remove")
+    button.setAttribute("type","button")
+    button.setAttribute("value","-")
+    new_item.appendChild(button)
+
+    text = document.createElement("p");
+    text.classlist.Add("Shop")
+    text.textContent = input;
+    new_item.appendChild(text)
+
+    
+    
+    
+    
+    
+    
+
+
+
+    
     
 }
